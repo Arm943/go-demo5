21eaf9a1628363cdbd9027b70ed76d1728fad6ef
ArmenTs <stalin943@gmail.com>
2025-04-21 19:02:11 +0300

    v1.1.0 выдает погоду в городе с проверкой правильности ввода города

--- geo/geo.go
+++ geo/geo.go
@@ -1,6 +1,7 @@
 package geo
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"io"
@@ -13,6 +14,10 @@ type GeoDate struct {
 
 func GetMyLocation(city string) (*GeoDate, error) {
 	if city != "" {
+		isCity := checkCity(city)
+		if !isCity {
+			panic("Такого города нет!")
+		}
 		return &GeoDate{
 			City: city,
 		}, nil
@@ -24,14 +29,36 @@ func GetMyLocation(city string) (*GeoDate, error) {
 	if resp.StatusCode != 200 {
 		return nil, errors.New("NOT200")
 	}
+	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	var geo GeoDate
 	json.Unmarshal(body, &geo)
-	if err != nil {
-		return nil, err
-	}
 	return &geo, nil
+}
+
+type CityPopulationResponce struct {
+	Error bool `json:"error"`
+}
+
+func checkCity(city string) bool {
+	postBody, _ := json.Marshal(map[string]string{
+		"city": city,
+	})
+
+	resp, err := http.Post("https://countriesnow.space/api/v0.1/countries/population/cities", "application/json", bytes.NewBuffer(postBody))
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+	var populationResponce CityPopulationResponce
+	json.Unmarshal(body, &populationResponce)
+	return !populationResponce.Error
 }
